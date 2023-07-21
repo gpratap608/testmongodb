6c5a9b6365eb9bdd8b1b9deb1e07fd07e3feca72
gpratap608 <113552039+gpratap608@users.noreply.github.com>
2023-07-21 19:24:37 +0530

    1.0,0

---
+++ my-magic-website/pages/index.js
@@ -0,0 +1,54 @@
+import { useState } from 'react';
+
+export default function Home() {
+  const [inputData, setInputData] = useState({
+    name:"",
+    age:""
+  });
+
+  const handleSaveData = async () => {
+    const response = await fetch('/api/savedata', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ Name: inputData.name, age: inputData.age }),
+    });
+
+    if (response.ok) {
+      alert('Data saved successfully!');
+      setInputData({
+        name:'',
+        age:""
+      });
+    } else {
+      alert('Something went wrong!');
+    }
+  };
+
+  const handleName = (event)=> {
+    const value = event.target.value
+    setInputData (values => ({...values,name:value}))
+  }
+
+  const handleAge = (event)=> {
+    const value = event.target.value
+    setInputData (values => ({...values,age:value}))
+  }
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={inputData.name}
+        onChange={handleName}
+      />
+      <input 
+        type="text"
+        value={inputData.age}
+        onChange={handleAge}
+      />
+      <button onClick={handleSaveData}>Save Data</button>
+    </div>
+  );
+}
